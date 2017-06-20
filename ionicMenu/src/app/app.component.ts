07a339b97c022b7a49b080e80ca5fae8730c35f4
Nata Silva <Nata Silva>
2017-06-19 21:29:19 -0300

    inicio da criação do menu

--- ionicMenu/src/app/app.component.ts
+++ ionicMenu/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { AboutPage } from './../pages/about/about';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -7,16 +8,28 @@ import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
+export class MyApp {  
+  
+  @ViewChild(Nav) nav: Nav; 
+  pages:[{title: string, component: any}];
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    
+    this.pages = [
+      {title: "Home page", component: HomePage}, 
+      {title: "About page", component: AboutPage}
+    ]
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+  openPage(page: {title: string, component: any}): void{
+      this.nav.setRoot(page.component);
   }
 }
 
